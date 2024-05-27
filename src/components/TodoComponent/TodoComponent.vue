25ce42d8b0ef6d81e24f77d84dc77217f275a94e
mameru-carr <samuelcarr.official@gmail.com>
2024-05-27 16:27:47 +0530

    Add Edit Mode Buttons!

--- src/components/TodoComponent/TodoComponent.vue
+++ src/components/TodoComponent/TodoComponent.vue
@@ -1,16 +1,38 @@
 <template>
     <div class="todo">
         <p class="todo-title">
-            <input type="checkbox" v-model="done" checked="{{ done }}" @click="toggleCompleted"/>
-            {{ props.todo.title }}
+            <template v-if="editMode">
+                <div class="edit-todo">
+                    <label><input v-model="done" checked="{{ done }}" type="checkbox" @click="toggleCompleted"/> Mark As
+                    Completed </label>
+                    <label>Title: </label> <input v-model="title" type="text"/>
+                    <label>Description: </label> <input v-model="description" type="text"/>
+                    <button class="save-btn" @click="editTodo">Save</button>
+                    <button class="cancel-btn" @click="toggleEditMode">Cancel</button>
+                </div>
+            </template>
+            <template v-else>
+                <div class="todo-list-item">
+                    <label class="completed-label">
+                    <input v-model="done" checked="{{ done }}" class="completed-checkbox"
+                           type="checkbox" @click="toggleCompleted"/>
+                    Mark As Completed
+                </label>
+                    <p class="title">{{ todo.title }}</p>
+                    <p class="description">{{ todo.description }}</p>
+                    <button class="edit-btn" @click="toggleEditMode">Edit</button>
+                    <!--                <button @click="deleteTodo">Delete</button>-->
+                </div>
+            </template>
         </p>
     </div>
 </template>
 
 <script setup>
 
-import { ref } from "vue";
+import {ref} from "vue";
 import axios from "axios";
+
 const base_url = "http://localhost:5035/api/todos";
 
 const props = defineProps({
@@ -26,18 +48,54 @@ const props = defineProps({
     }
 });
 
+const editMode = ref(false);
+
 const done = ref(props?.todo.done);
+const completedOn = ref(new Date()); // change later
+const title = ref(props?.todo.title);
+const description = ref(props?.todo.description);
+const todo = ref({
+    id: props?.todo.id,
+    title: props?.todo.title,
+    description: props?.todo.description,
+    done: props?.todo.done,
+    // completedOn: props?.todo.completedOn,
+});
+
+function toggleEditMode() {
+    editMode.value = !editMode.value;
+}
+
+function editTodo() {
+
+    todo.value = {
+        ...todo.value,
+        title: title.value,
+        description: description.value,
+        done: done.value,
+    }
+    axios.put(base_url, todo.value);
+
+    editMode.value = false;
+}
+
 function toggleCompleted() {
     // Toggle
     done.value = !done.value;
+    todo.value = {
+        ...todo.value,
+        done: done
+    }
+    if (done.value) {
+        completedOn.value = new Date();
+    }
 
     // Update in DB
-    axios.put(base_url, {
-        id: props?.todo.id,
-        description: props?.todo.description,
-        done: done.value,
-        title: props?.todo.title,
-    });
+    axios.put(base_url, todo.value);
 }
 
 </script>
+
+<style>
+
+</style>
